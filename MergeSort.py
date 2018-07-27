cb9ed32013be4348d2854fbc747f6f4c1f7c9601
nitin.nikam16@gmail.com <nitin.nikam16@gmail.com>
2018-07-27 15:49:24 +0530

    test reverse commit

--- MergeSort.py
+++ MergeSort.py
@@ -1,6 +1,3 @@
-"""
- This Program will do the Merge Sort using recursive Way
-"""
 def mergeSort(data):
     print("Splitting ",data)
     if len(data)>1 :
--- TestPythonCode.py
+++
@@ -1,7 +0,0 @@
-import re 
-
-def PrintMessage(msg):
-    print("Hey you entered the-{}".format(msg))
-
-
-printMessage("Welcome to Pune")
